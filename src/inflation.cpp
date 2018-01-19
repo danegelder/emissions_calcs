738c189f0067da78383e1333d0ad68caddc8831c
Ubuntu <ubuntu@ip-10-253-128-133.ap-southeast-2.compute.internal>
2018-01-19 02:06:59 +0000

    Added seperate program for inflationary emissions simulation

--- src/inflation.cpp
+++ src/inflation.cpp
@@ -5,12 +5,14 @@
 using namespace std;
 
 double InitialCirculatingSupply = 40000000.0;
-double EmissionSpeed = 22;
+double EmissionSpeed = 21;
+double InflationFactor = 0.000000027;
 double TotalSupply = (double) 120000000.0;
 double EmissionFactor; 
 double BlockReward;
 double CirculatingSupply = 0.0;
-double TailEmission = 3.5;
+double TailEmission = 0.5;
+double TotalBlocksToSimulate = 26298001;
 int ThisMonth;
 int i;
 
@@ -22,11 +24,15 @@ int main () {
     EmissionFactor = pow( (double) 2.0 , (double) - EmissionSpeed );
     CirculatingSupply = InitialCirculatingSupply;
     cout <<  "Month,Block,Reward,CirculatingSupply\n";
-    for (i = 1; i < 13149001; i++) {
-        BlockReward =  ( TotalSupply - CirculatingSupply ) * EmissionFactor;
-        if ( BlockReward < TailEmission) {
-	    BlockReward = TailEmission;
+    for (i = 0; i < TotalBlocksToSimulate ; i++) {
+	if ( CirculatingSupply > TotalSupply ) {
+            BlockReward =  CirculatingSupply  * InflationFactor; 
+	} else {
+            BlockReward =  (( TotalSupply - CirculatingSupply ) * EmissionFactor) + (CirculatingSupply  * InflationFactor);
 	}
+        //if ( BlockReward < TailEmission) {
+	//    BlockReward = TailEmission;
+	//}
 	CirculatingSupply += BlockReward;
 	if ((i % 21915) == 0) {
             ThisMonth = i / 21915;
